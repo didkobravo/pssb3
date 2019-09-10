d280924a978357bfd5c956351b75ec167f57ec35
didkobravo <didkobravo@gmail.com>
2019-09-10 14:00:35 +0300

    1 file modified, 1 added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+export const routes: Routes = [
+  { path: '', redirectTo: '/home' }
 ];
 
 @NgModule({
