7cb8c2a9b0ac88a5653900735d026cf78389b081
didkobravo <didkobravo@gmail.com>
2019-09-10 13:30:28 +0300

    3 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { FormsModule } from '@angular/forms';
 import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './common/app-header/app-header.component';
+
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule, FormsModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    ClarityModule, 
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    AppHeaderComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
